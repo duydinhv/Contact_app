c7b50b32455aa45f91bb861c55dfacd5f30ec2f9
duydinhv <53370450+duydinhv@users.noreply.github.com>
2020-09-17 09:40:22 +0700

    Initial Commit

---
+++ Contact_app_withRealm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contact_app_withRealm
+//
+//  Created by DUY on 9/17/20.
+//  Copyright © 2020 DUY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
